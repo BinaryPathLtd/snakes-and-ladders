de79b8d6f3a365011a2a1d066b1173c1bab40d97
Peter Arnold <PArnold@boden.co.uk>
2019-01-31 13:52:17 +0000

    Added second passing player can move test

--- src/SnakesAndLadders/Tests/TokenCanMoveFeature.cs
+++ src/SnakesAndLadders/Tests/TokenCanMoveFeature.cs
@@ -4,7 +4,10 @@ using NUnit.Framework;
 
 namespace Tests
 {
-    public class Feature1
+    [Description(@"As a player
+    I want to be able to move my token
+    So that I can get closer to the goal")]
+    public class TokenCanMoveFeature
     {
         [Test]
         public void GivenGameStarted_WhenTokenPlacedOnBoard_ThenTokenIsOnSquareOne()
@@ -35,6 +38,23 @@ namespace Tests
 
             //ThenTokenIsOnSquareFour
             Assert.That(game.Players?.First().TokenPosition, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void GivenPlayerTokenIsOnSquareOne_WhenTokenMoved3Spaces_AndMoved4Space_ThenTokenPositionIsOnSquareEight()
+        {
+            //GivenPlayerTokenIsOnSquare1
+            var boardSize = 100;
+            var playerCount = 1;
+            var game = new Game(boardSize, playerCount);
+            game.Start();
+            Assert.That(game.Players?.First().TokenPosition, Is.EqualTo(1));
+            //WhenTokenMoved3Spaces
+            game.Players?.First().Move(3);
+            //AndMoved4Space
+            game.Players?.First().Move(4);
+            //ThenTokenPositionIsOnSquareEight
+            Assert.That(game.Players?.First().TokenPosition, Is.EqualTo(8));
         }
     }
 }
